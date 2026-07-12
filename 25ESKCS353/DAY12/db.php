eb64fcaf1cafa008e9aba47129f67316b875edff
Tejas Garg <tejasgkg@gmail.com>
2026-07-12 23:16:19 +0530

    Update db.php

--- 25ESKCS353/DAY12/db.php
+++ 25ESKCS353/DAY12/db.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$db_user = "root"; // Default phpMyAdmin user
+$db_pass = "";     // Default phpMyAdmin password is empty
+$db_name = "student_management";
 
+$conn = new mysqli($host, $db_user, $db_pass, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
